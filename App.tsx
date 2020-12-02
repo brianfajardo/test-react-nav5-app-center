6dbbefd91fa89690a857eefe14037a2026d4b1e6
Brian Fajardo <brian.fajardo@ubisoft.com>
2020-12-01 23:39:45 -0500

    feat: install react navigation v5 dependencies
    native, stack, bottom tabs, drawer, gesture handler, reanimated, screens, safe area context and
    masked view

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -46,8 +47,8 @@ const App = () => {
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Step One</Text>
               <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-                screen and then come back to see your edits.
+                Edit <Text style={styles.highlight}>App.tsx</Text> to change
+                this screen and then come back to see your edits.
               </Text>
             </View>
             <View style={styles.sectionContainer}>
