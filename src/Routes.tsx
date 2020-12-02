e9b9ded40a986efc962a648fee32826ec93a592d
Brian Fajardo <brian.fajardo@ubisoft.com>
2020-12-02 00:06:03 -0500

    feat(routes): add register screen
    created RegisterScreen component and added it to routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,6 +3,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 
 import {LoginScreen} from './screens/LoginScreen';
+import {RegisterScreen} from './screens/RegisterScreen';
 
 interface RoutesProps {}
 
@@ -12,7 +13,8 @@ export const Routes: React.FC<RoutesProps> = ({}) => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/screens/RegisterScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+import {Center} from '../components/Center';
+
+interface RegisterScreenProps {}
+
+export const RegisterScreen: React.FC<RegisterScreenProps> = ({}) => {
+  return (
+    <Center>
+      <Text>Register Screen</Text>
+    </Center>
+  );
+};
