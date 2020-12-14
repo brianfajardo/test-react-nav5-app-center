1dcd32d8348bd64ce57cd71e943cc8101950a5d4
Brian Fajardo <bmf5@hotmail.com>
2020-12-13 19:13:09 -0500

    feat(feed screen): add flatlist with fake data

--- src/screens/FeedScreen.tsx
+++ src/screens/FeedScreen.tsx
@@ -1,12 +1,37 @@
 import React, { useContext } from 'react'
-import { Text } from 'react-native'
+import faker from 'faker'
 import { CrashAnalyticsContext } from '../contexts/CrashAnalytics'
-import { Center } from '../components/Center'
 import { CrashAlertModal } from '../components/CrashAlertModal'
+import { FlatList } from 'react-native-gesture-handler'
+import { StyleSheet } from 'react-native'
+import { Button } from '../components/Button'
+
+const FLATLIST_ROW_HEIGHT = 42
 
 export const FeedScreen: React.FC = () => {
   const { lastSessionCrashed, resetLastSessionCrashed } = useContext(
     CrashAnalyticsContext,
+  )
+
+  const flatListFakeData: Faker.UserCard[] = Array.from(Array(1000), () =>
+    faker.helpers.userCard(),
+  )
+
+  const flatListKeyExtractor = (user: Faker.UserCard, idx: number) =>
+    `${idx}-${user.username}`
+
+  const flatListGetItemLayout = (_data, idx: number) => ({
+    length: FLATLIST_ROW_HEIGHT,
+    offset: FLATLIST_ROW_HEIGHT * idx,
+    index: idx,
+  })
+
+  const flatListRenderItem = ({ item }: { item: Faker.UserCard }) => (
+    <Button
+      title={item.name}
+      onPress={() => console.log('press')}
+      style={styles.flatListRow}
+    />
   )
 
   return (
@@ -15,9 +40,24 @@ export const FeedScreen: React.FC = () => {
         visible={lastSessionCrashed}
         onConfirm={resetLastSessionCrashed}
       />
-      <Center>
-        <Text>Feed Screen</Text>
-      </Center>
+      <FlatList
+        data={flatListFakeData}
+        initialNumToRender={18}
+        getItemLayout={flatListGetItemLayout}
+        keyExtractor={flatListKeyExtractor}
+        maxToRenderPerBatch={15}
+        renderItem={flatListRenderItem}
+        removeClippedSubviews={true}
+        style={styles.flatList}
+        windowSize={3}
+      />
     </>
   )
 }
+
+const styles = StyleSheet.create({
+  flatList: {
+    width: '100%',
+  },
+  flatListRow: { height: FLATLIST_ROW_HEIGHT, flex: 1, borderRadius: 0 },
+})
