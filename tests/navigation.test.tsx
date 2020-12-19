56bc2cdc784d51a79a137011c06cc16a53ac02d0
Brian Fajardo <bmf5@hotmail.com>
2020-12-19 17:59:21 -0500

    test: refactor tests for navigation - auth

--- tests/navigation.test.tsx
+++ tests/navigation.test.tsx
@@ -1,35 +1,49 @@
 import React from 'react'
-import {
-  render,
-  waitForElementToBeRemoved,
-} from '@testing-library/react-native'
-import { App } from '../src/App'
-import AsyncStorage from '@react-native-community/async-storage'
+import { render } from '@testing-library/react-native'
+import { Routes } from '../src/Routes'
+import { AuthContext } from '../src/contexts/AuthProvider'
+import { act } from 'react-test-renderer'
 
 describe('Navigation', () => {
   describe('auth', () => {
-    beforeEach(async () => await AsyncStorage.clear())
+    it('should navigate to Login screen if the user is not authenticated', async () => {
+      const providerProps = {
+        loading: false,
+        user: null,
+        login: () => {},
+        logout: () => {},
+      }
 
-    const loadingMsg = 'One sec, making sure you exist.'
+      const wrapper = ({ children }) => (
+        <AuthContext.Provider value={providerProps}>
+          {children}
+        </AuthContext.Provider>
+      )
 
-    it('should render the Login screen if the user is not authenticated', async () => {
-      const screen = render(<App />)
+      const screen = render(<Routes />, { wrapper })
 
-      await waitForElementToBeRemoved(() => screen.getByText(loadingMsg))
-
-      expect(screen.getByText('Login Screen')).toBeDefined()
+      await act(async () =>
+        expect(screen.getByText('Login Screen')).toBeDefined(),
+      )
     })
 
-    it('should render the Home screen if the user is authenticated', async () => {
-      //  FIXME: Adding await causes console error to appear:
-      //  "Warning: You called act(async () => ...) without await."
-      AsyncStorage.setItem('user', JSON.stringify({ username: 'Santa' }))
+    it('should navigate to Friends screen if the user is authenticated', async () => {
+      const providerProps = {
+        loading: false,
+        user: { username: 'Santa' },
+        login: () => {},
+        logout: () => {},
+      }
 
-      const screen = render(<App />)
+      const wrapper = ({ children }) => (
+        <AuthContext.Provider value={providerProps}>
+          {children}
+        </AuthContext.Provider>
+      )
 
-      await waitForElementToBeRemoved(() => screen.getByText(loadingMsg))
+      const screen = render(<Routes />, { wrapper })
 
-      expect(screen.getByText('Friends')).toBeDefined()
+      await act(async () => expect(screen.getByText('Friends')).toBeDefined())
     })
   })
 })
