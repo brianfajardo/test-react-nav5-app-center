32bc8a68228c500fb46d9bd51de8c58249fcb359
Brian Fajardo <bmf5@hotmail.com>
2020-12-19 18:59:27 -0500

    test: add tests for bottom tab navigation

--- tests/navigation.test.tsx
+++ tests/navigation.test.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import { render } from '@testing-library/react-native'
-import { Routes } from '../src/Routes'
+import { render, cleanup, fireEvent } from '@testing-library/react-native'
+import { AppNavigator } from '../src/AppNavigator'
 import { AuthContext } from '../src/contexts/AuthProvider'
-import { act } from 'react-test-renderer'
+import { NavigationContainer } from '@react-navigation/native'
+
+afterEach(cleanup)
 
 describe('Navigation', () => {
   describe('auth', () => {
@@ -16,15 +18,14 @@ describe('Navigation', () => {
 
       const wrapper = ({ children }) => (
         <AuthContext.Provider value={providerProps}>
-          {children}
+          <NavigationContainer>{children}</NavigationContainer>
         </AuthContext.Provider>
       )
 
-      const screen = render(<Routes />, { wrapper })
+      const screen = render(<AppNavigator />, { wrapper })
+      const loginScreenTitle = await screen.findByText('Login Screen')
 
-      await act(async () =>
-        expect(screen.getByText('Login Screen')).toBeDefined(),
-      )
+      expect(loginScreenTitle).toBeTruthy()
     })
 
     it('should navigate to Friends screen if the user is authenticated', async () => {
@@ -37,13 +38,66 @@ describe('Navigation', () => {
 
       const wrapper = ({ children }) => (
         <AuthContext.Provider value={providerProps}>
-          {children}
+          <NavigationContainer>{children}</NavigationContainer>
         </AuthContext.Provider>
       )
 
-      const screen = render(<Routes />, { wrapper })
+      const screen = render(<AppNavigator />, { wrapper })
+      const friendsScreenTitle = await screen.findByText('Friends')
 
-      await act(async () => expect(screen.getByText('Friends')).toBeDefined())
+      expect(friendsScreenTitle).toBeDefined()
+    })
+  })
+
+  describe('bottom tabs', () => {
+    it('should navigate to Friends screen when the home tab is pressed', async () => {
+      const providerProps = {
+        loading: false,
+        user: { username: 'Santa' },
+        login: () => {},
+        logout: () => {},
+      }
+
+      const wrapper = ({ children }) => (
+        <AuthContext.Provider value={providerProps}>
+          <NavigationContainer>{children}</NavigationContainer>
+        </AuthContext.Provider>
+      )
+
+      const screen = render(<AppNavigator />, { wrapper })
+      const homeTab = await screen.findByText('Home')
+
+      fireEvent(homeTab, 'press')
+
+      const friendsScreenTitle = screen.getByText('Friends')
+
+      expect(homeTab).toBeTruthy()
+      expect(friendsScreenTitle).toBeTruthy()
+    })
+
+    it('should navigate to Search screen when the search tab is pressed', async () => {
+      const providerProps = {
+        loading: false,
+        user: { username: 'Santa' },
+        login: () => {},
+        logout: () => {},
+      }
+
+      const wrapper = ({ children }) => (
+        <AuthContext.Provider value={providerProps}>
+          <NavigationContainer>{children}</NavigationContainer>
+        </AuthContext.Provider>
+      )
+
+      const screen = render(<AppNavigator />, { wrapper })
+      const searchTab = await screen.findByText('Search')
+
+      fireEvent(searchTab, 'press')
+
+      const searchScreenTitle = screen.getByText('Search Screen')
+
+      expect(searchTab).toBeTruthy()
+      expect(searchScreenTitle).toBeTruthy()
     })
   })
 })
